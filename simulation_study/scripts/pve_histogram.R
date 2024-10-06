62c2d34ba59f62b66c14789960a21b74eb203a54
Jeff Okamoto <jokamoto@umich.edu>
2024-10-06 13:54:34 -0400

    eqtl pve (10/6/2024)

--- simulation_study/scripts/pve_histogram.R
+++ simulation_study/scripts/pve_histogram.R
@@ -1,6 +1,7 @@
 #Make histogram to visualize distribution of PVE for GWAS simulation
 
 library(ggplot2)
+library(dplyr)
 
 pve <- read.table("sim_data/gwas_pve.txt")
 
@@ -13,6 +14,29 @@ ggplot(pve,aes(x = V1)) +
         ylab("Number of Simulated GWAS Traits") +
         geom_vline(xintercept = mean_pve,color = "red",alpha=0.5) +
         annotate("text", label = paste0("Mean PVE = ",round(mean_pve,digits=2)), x = 0.3, y = 40, alpha = 0.5,size = 8, colour = "red") +
+        theme_bw() +
+        theme(text = element_text(size=10,face="bold"),aspect.ratio=1)
+dev.off()
+
+#Make histogram to visualize distribution of PVE for eQTL simulation
+
+pve <- read.table("sim_data/eqtl.truth") %>%
+        dplyr::select(V1,V4) %>%
+        unique()
+
+mean_pve <- mean(pve$V4)
+
+#600 region datasets
+pve <- do.call("rbind", replicate(600, pve, simplify = FALSE))
+
+pdf("sim_rst/expr_pve.pdf")
+pve %>%
+        ggplot(aes(x = V4)) +
+        geom_histogram(binwidth=0.05) +
+        xlab("PVE") +
+        ylab("Number of Simulated Genes") +
+        geom_vline(xintercept = mean_pve,color = "red",alpha=0.5) +
+        annotate("text", label = paste0("Mean Expression PVE = ",round(mean_pve,digits=2)), x = 0.3, y = 5000, alpha = 0.5,size = 6, colour = "red") +
         theme_bw() +
         theme(text = element_text(size=10,face="bold"),aspect.ratio=1)
 dev.off()
